bbc22c56c1fd5e85d4bc8809795204e86d2c4201
ApinisAtvars <atvars.apinis@student.howest.be>
2026-03-12 13:13:48 -0300

    CLEAN: Remove GMF and MLP configs, i won't run them individually

--- NCF/train.py
+++ NCF/train.py
@@ -1,51 +1,7 @@
 import pandas as pd
 import numpy as np
-from gmf import GMFEngine
-from mlp import MLPEngine
 from neumf import NeuMFEngine
 from data import SampleGenerator
-
-gmf_config = {'alias': 'gmf_factor8neg4-implict',
-              'num_epoch': 200,
-              'batch_size': 1024,
-              # 'optimizer': 'sgd',
-              # 'sgd_lr': 1e-3,
-              # 'sgd_momentum': 0.9,
-              # 'optimizer': 'rmsprop',
-              # 'rmsprop_lr': 1e-3,
-              # 'rmsprop_alpha': 0.99,
-              # 'rmsprop_momentum': 0,
-              'optimizer': 'adam',
-              'adam_lr': 1e-3,
-              'num_users': 6040,
-              'num_items': 3706,
-              'latent_dim': 8,
-              'num_negative': 4,
-              'l2_regularization': 0,  # 0.01
-              'weight_init_gaussian': True,
-              'use_cuda': True,
-              'use_bachify_eval': False,
-              'device_id': 0,
-              'model_dir': 'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
-
-mlp_config = {'alias': 'mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001',
-              'num_epoch': 200,
-              'batch_size': 256,  # 1024,
-              'optimizer': 'adam',
-              'adam_lr': 1e-3,
-              'num_users': 6040,
-              'num_items': 3706,
-              'latent_dim': 8,
-              'num_negative': 4,
-              'layers': [16, 64, 32, 16, 8],  # layers[0] is the concat of latent user vector & latent item vector
-              'l2_regularization': 0.0000001,  # MLP model is sensitive to hyper params
-              'weight_init_gaussian': True,
-              'use_cuda': True,
-              'use_bachify_eval': False,
-              'device_id': 0,
-              'pretrain': False,
-              'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4_Epoch100_HR0.6391_NDCG0.2852.model'),
-              'model_dir': 'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
 
 neumf_config = {'alias': 'neumf_factor8neg4',
                 'num_epoch': 1,
